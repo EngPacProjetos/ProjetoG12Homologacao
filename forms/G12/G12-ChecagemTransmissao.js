2d1e2f3f06cbb61089cafacf30866783721e7ce8
Enos Rocha <ens4562@gmail.com>
2026-08-10 08:40:01 -0300

    ALTERACAO DAS FUNCOES DE CHACGEMN DE TRANSMISSAO PARA APORTAR UMA NOVA FASE DE CHECAGEM NO AGUARDANDO RECEBIMENTO

--- forms/G12/G12-ChecagemTransmissao.js
+++ forms/G12/G12-ChecagemTransmissao.js
@@ -76,3 +76,83 @@ function selecionarBotaoTransmissaoSetor(botao) {
     }
 
 }
+
+
+function selecionarBotaoTransmissaoRecebimento(botao) {
+
+    $("#infoSetorAjuste").val("");
+
+    var id = botao.id;
+
+    if (id == "infoNfseCorretasRecebimento") {
+        $(botao).css("background", "#1eaad9");
+        $(botao).css("color", "white");
+
+        var siblings = $(botao).siblings();
+
+        $(siblings).css("background", "none");
+        $(siblings).css("color", "black");
+
+        var infoTransmissaoCorreta = $("#infoTransmissaoCorreta").val("sim");
+
+        $("#financeiroReponsavelRecebimento").css("pointer-events", "none")
+        $("#tecnicoReponsavelRecebimento").css("pointer-events", "none")
+        $("#financeiroReponsavelRecebimento").css("background", "#e9ecef")
+        $("#tecnicoReponsavelRecebimento").css("background", "#e9ecef")
+        $("#financeiroReponsavelRecebimento").css("color", "#6c757d")
+        $("#tecnicoReponsavelRecebimento").css("color", "#6c757d")
+        $("#ajusteTransmissaoRecebimento").css("pointer-events", "none");
+        $("#ajusteTransmissaoRecebimento").css("background", "#e9ecef");
+    }
+    if (id == "infoNfseErradasRecebimento") {
+        $(botao).css("background", "#1eaad9");
+        $(botao).css("color", "white");
+
+        var siblings = $(botao).siblings();
+
+        $(siblings).css("background", "none");
+        $(siblings).css("color", "black");
+
+        var infoTransmissaoCorreta = $("#infoTransmissaoCorreta").val("nao");
+
+        $("#financeiroReponsavelRecebimento").css("pointer-events", "auto")
+        $("#tecnicoReponsavelRecebimento").css("pointer-events", "auto")
+        $("#financeiroReponsavelRecebimento").css("background", "none")
+        $("#tecnicoReponsavelRecebimento").css("background", "none")
+        $("#financeiroReponsavelRecebimento").css("color", "#000000")
+        $("#tecnicoReponsavelRecebimento").css("color", "#000000")
+        $("#ajusteTransmissaoRecebimento").css("pointer-events", "auto");
+        $("#ajusteTransmissaoRecebimento").css("background", "none");
+    }
+
+}
+function selecionarBotaoTransmissaoSetorRecebimento(botao) {
+
+    var id = botao.id;
+
+    if (id == "financeiroReponsavelRecebimento") {
+        $(botao).css("background", "#1eaad9");
+        $(botao).css("color", "white");
+
+        var siblings = $(botao).siblings();
+
+        $(siblings).css("background", "none");
+        $(siblings).css("color", "black");
+
+        var infoSetorAjuste = $("#infoSetorAjuste").val("financeiro");
+        console.log(infoSetorAjuste)
+    }
+    if (id == "tecnicoReponsavelRecebimento") {
+        $(botao).css("background", "#1eaad9");
+        $(botao).css("color", "white");
+
+        var siblings = $(botao).siblings();
+
+        $(siblings).css("background", "none");
+        $(siblings).css("color", "black");
+
+        var infoSetorAjuste = $("#infoSetorAjuste").val("tecnico");
+        console.log(infoSetorAjuste)
+    }
+
+}
